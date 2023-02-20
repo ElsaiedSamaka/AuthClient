3102cdefa65aa039e380805459f42d92e056f614
unknown <elsaied.samaka1997@gmail.com>
2023-02-20 14:45:25 +0200

    wrap up signup & check signedin functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from './auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AuthClient';
+  signedin$: BehaviorSubject<boolean>;
+  // we need to inject the AuthService into our app component
+  // we also need to subscribe to the signedin$ subject
+  constructor(private authService: AuthService) {
+    this.signedin$ = authService.signedin$;
+    // console.log(authService.signedin$.value);
+  }
+  // whenever our app component is initialized, we want to check if the user is authenticated
+  // if they are, we should set the signedin$ subject to true
+  ngOnInit(): void {
+    this.authService.checkAuth().subscribe();
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 
 interface EmailAvailableResponse {
   message: string;
@@ -10,15 +11,23 @@ interface EmailAvailableResponse {
 })
 export class AuthService {
   rootUrl = 'http://localhost:3000';
+  signedin$ = new BehaviorSubject<boolean>(false);
+
   constructor(private http: HttpClient) {}
+
+  // emailAvailable will be called whenever the user types in the email field
+  // we will send the email to the server and check if it is available
   emailAvailable(email: string) {
     return this.http.post<EmailAvailableResponse>(
       `${this.rootUrl}/api/auth/username`,
       {
         email: email,
-      }
+      },
+      { withCredentials: true }
     );
   }
+  // signup will be called when the user submits the signup form
+  // we will send the user's credentials to the server
   signup(
     firstname: string,
     lastname: string,
@@ -27,13 +36,35 @@ export class AuthService {
     passwordConfirmation: string,
     phonenumber: string
   ) {
-    return this.http.post<any>(`${this.rootUrl}/api/auth/signup`, {
-      firstname: firstname,
-      lastname: lastname,
-      email: email,
-      password: password,
-      passwordConfirmation: passwordConfirmation,
-      phonenumber: phonenumber,
-    });
+    return this.http
+      .post<any>(
+        `${this.rootUrl}/api/auth/signup`,
+        {
+          firstname: firstname,
+          lastname: lastname,
+          email: email,
+          password: password,
+          passwordConfirmation: passwordConfirmation,
+          phonenumber: phonenumber,
+        },
+        { withCredentials: true }
+      )
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true);
+        })
+      );
+  }
+  // checkAuth will be called whenever our app component is initialized
+  // we can use this to check if the user is authenticated
+  checkAuth() {
+    return this.http
+      .get<any>(`${this.rootUrl}/api/auth/signedin`, { withCredentials: true })
+      .pipe(
+        tap((response) => {
+          // this.signedin$.next(authenticated);
+          console.log(response);
+        })
+      );
   }
 }
