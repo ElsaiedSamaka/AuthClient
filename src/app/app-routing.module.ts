ae77db882fc14eb604389604e304dd2d0f720f00
unknown <elsaied.samaka1997@gmail.com>
2023-02-20 22:08:30 +0200

    added index module plus home component -will be load on our app-routing-module as lazy loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    loadChildren: () =>
+      import('./index/index.module').then((m) => m.IndexModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
