6f38dab97f1e80bf26c2bce8bf217c9406338cf2
Saied <elsaied.samaka1997@gmail.com>
2023-04-09 12:16:13 +0200

    working break point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'index',
     // TODO: uncomment this below statement
-    // canLoad: [AuthGuard],
+    canLoad: [AuthGuard],
     loadChildren: () =>
       import('./index/index.module').then((m) => m.IndexModule),
   },
